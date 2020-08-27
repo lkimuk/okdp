9db5ada44ed85806f97e4f53f057478da2ab84ba
wavekey <ymiwli@hotmail.com>
2020-08-27 22:47:10 +0800

    singleton second commit

--- singleton/singleton.hpp
+++ singleton/singleton.hpp
@@ -2,7 +2,7 @@
  *
 MIT License
 
-Copyright (c) 2020 riancy
+Copyright (c) 2020 gxkey(Gaoxing Li)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -20,7 +20,7 @@ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+SOFTWARE. 
 */
 
 #ifndef OKDP_SINGLETON_HPP
