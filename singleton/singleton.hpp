0a9418cc89a97da2fc1d8f710a05a395af97311d
gxkey <ymiwli@hotmail.com>
2021-04-02 20:56:51 +0800

    singleton pattern last version

--- singleton/singleton.hpp
+++ singleton/singleton.hpp
@@ -2,7 +2,7 @@
  *
 MIT License
 
-Copyright (c) 2020 gxkey(Gaoxing Li)
+Copyright (c) 2020 lkimuk(Gaoxing Li)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
